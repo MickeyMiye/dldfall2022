fdfcbcff0677fcdad83e41847792a102e15a79e8
MickeyMiye <mcbrian.miye@okstate.edu>
2022-09-29 13:35:41 -0500

    updated lab1 files

--- Lab1/SV/silly.sv
+++ Lab1/SV/silly.sv
@@ -1,5 +1,15 @@
-module silly (input  logic a, b, c, output logic y);
+module fulladder (input logic a,b,c, output logic sum, y);
+  assign y = a & b | c & b| c & a;
+  assign sum = a ^ b ^ c;
+endmodule
+
+module rca (input  logic [3:0] a, b, input logic c, output logic [3:0]sum, output logic y);
    
-  assign y = ~b & ~c | a & ~b;
+   logic  y0, y1, y2;
+  fulladder g1(a[0], b[0], c, sum[0], y0);
+  fulladder g2(a[1], b[1], y0, sum[1], y1);
+  fulladder g3(a[2], b[2], y1, sum[2], y2);
+  fulladder g4(a[3], b[3], y2, sum[3], y);
+
    
 endmodule
--- Lab1/SV/silly_tb.sv
+++ Lab1/SV/silly_tb.sv
@@ -1,61 +1,58 @@
 `timescale 1ns / 1ps
 module tb ();
 
-   logic        a;
-   logic 	b;
+   logic [3:0]       a;
+   logic [3:0]	b;
    logic 	c;
    logic 	y;
-   logic        clk;   
+   logic [3:0]   sum;
+   logic        clk; 
+   logic [3:0] Sum_correct;
+     
    
   // instantiate device under test
-   silly dut (a, b, c, y);
+   rca dut (a, b, c, sum, y);
 
-   // 2 ns clock
-   initial 
-     begin	
-	clk = 1'b1;
-	forever #10 clk = ~clk;
-     end
-
-
-   initial
-     begin
-    
-	#0   a = 1'b0;	
-	#0   b = 1'b0;
-	#0   c = 1'b0;
-
-	#20  a = 1'b1;
-	#0   b = 1'b0;
-	#0   c = 1'b0;
-
-	#20  a = 1'b0;
-	#0   b = 1'b1;
-	#0   c = 1'b0;
-
-	#20  a = 1'b1;
-	#0   b = 1'b1;
-	#0   c = 1'b0;
-
-	#0   a = 1'b0;	
-	#0   b = 1'b0;
-	#0   c = 1'b1;
-
-	#20  a = 1'b1;
-	#0   b = 1'b0;
-	#0   c = 1'b1;
-
-	#20  a = 1'b0;
-	#0   b = 1'b1;
-	#0   c = 1'b1;
-
-	#20  a = 1'b1;
-	#0   b = 1'b1;
-	#0   c = 1'b1;		
+  initial 
+	begin
+		clk = 1'b1;
+		forever #10 clk = ~clk;
+  end
 
 
-	
-     end
+ integer handle3; 
+ integer desc3; 
+ integer i;
+
+assign Sum_correct = a + b + c;
+
+initial begin
+handle3 = $fopen("rca.out"); 
+desc3 = handle3;
+ #5000 $finish;
+end
+
+initial 
+begin
+for (i=0; i < 150; i=i+1) 
+begin 
+// Put vectors before beginning of clk
+@(posedge clk)
+begin 
+a = $random;
+b = $random;
+c = 1'b0;
+end
+@(negedge clk)
+begin
+$fdisplay(desc3, "%h %h || %h | %h | %b", a, b, sum, Sum_correct, (sum == Sum_correct)); 
+end
+end
+
+ 
+
+end 
+
 
    
 endmodule
